5d5d1764d039c3454089a07f2f37355f149c3d8a
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2025-07-18 14:47:01 +0500

    clear

--- src/lib/socket.ts
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-// ⚠️ server URL ni backendga qarab o‘zgartiring
-export const socket = io("http://localhost:7070", {
-  withCredentials: true,
-  autoConnect: false,
-});
